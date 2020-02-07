6e97dfdd9f17e6012be4f69a2e6d627b898917df
Alberto González Balderas <albertoglezb@hotmail.com>
2020-02-07 09:30:21 -0600

    Se cambio el arreglo por coleccion y se crearon varios metodos de uso de la coleccion. Se termino la clase: 'Implementando colecciones'.

--- Entidades/Escuela.cs
+++ Entidades/Escuela.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace CoreEscuela.Entidades
 {
     class Escuela
@@ -18,7 +20,7 @@ namespace CoreEscuela.Entidades
 
         public TiposEscuela TipoEscuela { get; set; }
 
-        public Curso[] Cursos { get; set; }
+        public List<Curso> Cursos { get; set; }
 
         #region Constructors
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using CoreEscuela.Entidades;
 using static System.Console;
 
@@ -10,20 +11,28 @@ namespace CoreEscuela
         {
             var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria, ciudad: "Bogota", pais: "Colombia");
 
-            escuela.Cursos = new Curso[]  {
+            escuela.Cursos = new List<Curso>(){
                 new Curso() { Nombre = "101" },
                 new Curso() { Nombre = "201" },
                 new Curso { Nombre = "301" }
             };
 
+            escuela.Cursos.Add(new Curso { Nombre = "102", Jornada = TiposJornada.Tarde });
+            escuela.Cursos.Add(new Curso { Nombre = "202", Jornada = TiposJornada.Tarde });
+
+            var cursos = new List<Curso>(){
+                new Curso() { Nombre = "401" },
+                new Curso() { Nombre = "501" },
+                new Curso { Nombre = "502" }
+            };
+
+            cursos.Clear();
+
+            escuela.Cursos.AddRange(cursos);
+
             WriteLine(escuela);
             WriteLine(new String('*', 30));
-            ImprimirCursos(escuela.Cursos);
-
-            if (true)
-            {
-
-            }
+            ImprimirCursos(escuela.Cursos.ToArray());
 
         }
 
